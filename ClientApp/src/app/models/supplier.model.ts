bea93b78571182e96cbb42bef21e29abc05e6a80
Gulraiz Gulshan <gulraizgulshan@gmail.com>
2020-12-15 16:55:02 +0500

    changes to supplier model

--- ClientApp/src/app/models/supplier.model.ts
+++ ClientApp/src/app/models/supplier.model.ts
@@ -1,7 +1,10 @@
+import { Product } from './product.model';
+
 export class Supplier {
   constructor(
     public supplierId?: number,
     public name?: string,
     public city?: string,
-    public state?: string) { }
+    public state?: string,
+    public products?: Product[]) { }
 }
